582c833c2a18df4fd6649e1e72894db67c704d42
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-01-27 20:51:16 +0200

    adding ValidatorUtilImpl class and Bean configuration class with initial Gson @Bean, and some todos in validator class

---
+++ web/src/main/java/com/example/web/models/utils/BeanConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.web.models.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+//                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+    }
+}
---
+++ web/src/main/java/com/example/web/models/utils/ValidatorUtilImpl.java
@@ -0,0 +1,33 @@
+package com.example.web.models.utils;
+
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ValidatorUtilImpl implements ValidatorUtil {
+    private final Validator validator;
+
+    public ValidatorUtilImpl() {
+        this.validator = Validation
+                .buildDefaultValidatorFactory()
+                .getValidator();
+    }
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return this.validator.validate(entity).isEmpty();
+    }
+
+    @Override
+    public <E> boolean isAdmin(E entity) {
+        //todo: should I make this method static or call it through constructor
+        return false;
+    }
+
+    @Override
+    public <E> boolean isActive(E entity) {
+        //todo: should I make this method static or call it through constructor
+        return false;
+    }
+}
