79a32b5a6923d9b0d90f59348d153242c9705244
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-01-27 19:04:39 +0200

    first version of my web project

---
+++ web/src/main/java/com/example/web/models/repository/ToursRepository.java
@@ -0,0 +1,9 @@
+package com.example.web.models.repository;
+
+import com.example.web.models.entity.TourOfferEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToursRepository extends JpaRepository<TourOfferEntity, Long> {
+}
