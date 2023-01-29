e7e0bc3905a371f33387bc1a26ebe131d347dd8f
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-01-29 22:06:34 +0200

    changes

---
+++ web/src/main/java/com/example/web/TestWebAppRunner.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestWebAppRunner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
