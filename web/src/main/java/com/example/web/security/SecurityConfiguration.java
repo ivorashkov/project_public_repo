d1ccf7c33d428dfcedd55496d194d0b1b528d685
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-03-20 15:05:59 +0200

    corsFilter class created due to issue with /logout from spring security default implementation. Also added in SecurityConfiguration chain at LogoutFilter and it works fine

--- web/src/main/java/com/example/web/security/SecurityConfiguration.java
+++ web/src/main/java/com/example/web/security/SecurityConfiguration.java
@@ -1,17 +1,20 @@
 package com.example.web.security;
 
 import com.example.web.model.enums.RoleType;
+import com.example.web.security.cors.CorsFilter;
 import com.example.web.security.jwt.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
@@ -20,6 +23,7 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 public class SecurityConfiguration {
 
   private final JwtAuthFilter jwtAuthFilter;
+  private final CorsFilter corsFilter;
   private final AuthenticationProvider authenticationProvider;
   private final LogoutHandler logoutHandler;//LogoutService implimentation. Spring will find that we have impl
 
@@ -32,8 +36,7 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
-        .csrf()
-        .disable()
+        .csrf().disable()
         .authorizeHttpRequests()
         .requestMatchers(AUTH_WHITELIST)
         .permitAll()
@@ -49,6 +52,7 @@ public class SecurityConfiguration {
         .and()
         .authenticationProvider(authenticationProvider)
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        .addFilterAt(corsFilter, LogoutFilter.class)
         .logout()
         .logoutUrl("/api/auth/logout") //using default Spring logout without implimentation
         .addLogoutHandler(logoutHandler)
--- web/src/main/java/com/example/web/security/service/LogoutServiceImpl.java
+++ web/src/main/java/com/example/web/security/service/LogoutServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+
 public class LogoutServiceImpl implements LogoutService {
   private final TokenRepository tokenRepository;
 
