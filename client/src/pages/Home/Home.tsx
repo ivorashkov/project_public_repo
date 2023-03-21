48cad72a419f95f3cfbe087f2b178d2ba7d86cb4
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-03-21 21:08:11 +0200

    additional stuff added

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,63 +1,61 @@
-import { FormEvent, useEffect, useState } from "react";
-import { getAllOffersPaging } from '../../services';
+import {FormEvent, useEffect, useState} from "react";
+import {getAllOffersPaging} from '../../services';
 
 export const Home = () => {
   const [searchValue, setSearchValue] = useState<string>('');
-  const [offers, setOffers] = useState();
+  const [offers, setOffers] = useState([]);
 
   useEffect(() => {
     getAllOffersPaging().then(el => {
       setOffers(el.content)
-      console.log(el.content)
-      console.log(offers)
     })
   }, [])
-
   // useEffect(() => {
   //
   // }, [searchValue])
 
   return (
-    <section>
-      <h1>home</h1>
+      <section>
+        <h1>home</h1>
 
-      <div className="section__form">
-        <form action="">
-          <div className="form__row">
-            <label htmlFor="location">Location</label>
-            
-            <select name="location" id="location">
-              <input type="text" name="location" id="location" onChange={(e) =>setSearchValue(e.target.value) } />
-            </select>
-          </div>
+        <div className="section__form">
+          <form action="">
+            <div className="form__row">
+              <label htmlFor="location">Location</label>
 
-          <div className="form__row">
-            <label htmlFor="city">City</label>
+              <select name="location" id="location">
+                <input type="text" name="location" id="location"
+                       onChange={(e) => setSearchValue(e.target.value)}/>
+              </select>
+            </div>
 
-            <select name="city" id="city">
-              <option value="City">City</option>
-            </select>
-          </div>
-        </form>
-      </div>
-      
-      <div className="section__grid">
-        <div className="grid">
-          <div className="grid__items">
-            <ul>
-              {/*{content.map((of) => <li><div className="offer"></div></li> )}  */}
-            </ul>
+            <div className="form__row">
+              <label htmlFor="city">City</label>
+
+              <select name="city" id="city">
+                <option value="City">City</option>
+              </select>
+            </div>
+          </form>
+        </div>
+
+        <div className="section__grid">
+          <div className="grid">
+            <div className="grid__items">
+              <ul>
+                {offers.map((of) => <li><div className="offer"></div></li> )}
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="section__paging">
+        <div className="section__paging">
           <div className="paging">
             <ul>
               <li><a href=" "></a></li>
             </ul>
           </div>
-      </div>
-    </section>
+        </div>
+      </section>
   );
 };
