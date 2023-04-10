3ec01cfd3491d4a8131f174943aa3eea4e085821
Ivaylo Rashkov <ivaylorashkov@icloud.com>
2023-04-10 15:01:29 +0300

    adj

--- web/src/main/java/com/example/web/repository/AccountInfoRepository.java
+++ web/src/main/java/com/example/web/repository/AccountInfoRepository.java
@@ -1,7 +1,6 @@
 package com.example.web.repository;
 
 import com.example.web.model.entity.AccountInfoEntity;
-import java.nio.file.Path;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
